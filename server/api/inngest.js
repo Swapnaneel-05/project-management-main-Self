103e25e95a59a795ca71a6c7d41076de39ce6376
Swapnaneel-05 <swapnaneelbhadra@gmail.com>
2026-04-08 20:29:34 +0530

    inngest update

--- server/api/inngest.js
+++ server/api/inngest.js
@@ -1,6 +1,10 @@
 import { serve } from "inngest/edge";
 import { inngest, functions } from "../inngest/index.js";
 
+export const config = {
+  runtime: "edge",
+};
+
 export default serve({
   client: inngest,
   functions,
