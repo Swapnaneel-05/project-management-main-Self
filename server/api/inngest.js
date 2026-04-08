9ca493077b600e2f3586455637addf718d075293
Swapnaneel-05 <swapnaneelbhadra@gmail.com>
2026-04-08 20:34:21 +0530

    force node runtime for inngest

--- server/api/inngest.js
+++ server/api/inngest.js
@@ -1,6 +1,10 @@
 import { serve } from "inngest/node";
 import { inngest, functions } from "../inngest/index.js";
 
+export const config = {
+  runtime: "nodejs",
+};
+
 export default serve({
   client: inngest,
   functions,
