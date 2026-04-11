da783ef33d60cb2dcfcdaa38d99ec974a723637d
Swapnaneel-05 <swapnaneelbhadra@gmail.com>
2026-04-11 22:35:23 +0530

    deleted node export

--- server/api/inngest.js
+++ server/api/inngest.js
@@ -1,10 +1,6 @@
 import { serve } from "inngest/node";
 import { inngest, functions } from "../inngest/index.js";
 
-export const config = {
-  runtime: "nodejs",
-};
-
 export default serve({
   client: inngest,
   functions,
