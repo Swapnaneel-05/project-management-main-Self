66fa1c3cbd848bac32b1149ad0197f6e261d5a11
Swapnaneel-05 <swapnaneelbhadra@gmail.com>
2026-04-14 02:14:51 +0530

    cors fixes

--- server/server.js
+++ server/server.js
@@ -11,7 +11,21 @@ import commentRouter from "./routes/commentRoutes.js";
 
 const app = express();
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://project-management-main-self-front.vercel.app"
+  ],
+  credentials: true
+}));
+
+app.options("*", cors({
+  origin: [
+    "http://localhost:5173",
+    "https://project-management-main-self-front.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json())
 app.use(clerkMiddleware())
 
