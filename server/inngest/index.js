684807eb005a727363cbcc98f99889f5c996e536
Swapnaneel-05 <swapnaneelbhadra@gmail.com>
2026-04-13 23:28:25 +0530

    changed member creation

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -134,26 +134,15 @@ const syncWorkspaceMemberCreation = inngest.createFunction(
     id: "sync-workspace-member-from-clerk",
     triggers: [{ event: "clerk/organizationMembership.created" }],
   },
-  async ({ event, step }) => {
+  async ({ event }) => {
+    const { data } = event;
 
-    const data = event.data;
-
-    const userId = data.public_user_data?.user_id;
-    const workspaceId = data.organization?.id;
-    const role = (data.role || "member").toUpperCase();
-
-    if (!userId || !workspaceId) {
-      throw new Error("Missing userId or workspaceId");
-    }
-
-    await step.run("create-workspace-member", async () => {
-      await prisma.workspaceMember.create({
-        data: {
-          userId,
-          workspaceId,
-          role,
-        },
-      });
+    await prisma.workspaceMember.create({
+      data: {
+        userId: data.user_id,
+        workspaceId: data.organization_id,
+        role: (data.role_name || "member").toUpperCase(),
+      },
     });
   }
 );
