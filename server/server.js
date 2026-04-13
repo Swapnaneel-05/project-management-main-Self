f73abea3c045333ff9919dd472520beaa7fe2c6d
Swapnaneel-05 <swapnaneelbhadra@gmail.com>
2026-04-14 02:11:49 +0530

    cors error changes

--- server/server.js
+++ server/server.js
@@ -11,8 +11,8 @@ import commentRouter from "./routes/commentRoutes.js";
 
 const app = express();
 
-app.use(express.json())
 app.use(cors())
+app.use(express.json())
 app.use(clerkMiddleware())
 
 app.get("/",(req,res)=>{
