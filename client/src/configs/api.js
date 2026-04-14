64cd4002d9c01e2533fd957824e08786af62d6d9
Swapnaneel-05 <swapnaneelbhadra@gmail.com>
2026-04-14 23:12:20 +0530

    chnages

--- client/src/configs/api.js
+++ client/src/configs/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.RENDER_BASEURL || "http://localhost:5000"
+  baseURL: import.meta.env.RENDER_BASEURL || "https://project-management-main-self-1.onrender.com"
 })
 
 export default api
